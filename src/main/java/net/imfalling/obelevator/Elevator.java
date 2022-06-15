f32c0354912bf7d67fd04a25a6bc7d28be68967e
Zensonaton <zensonaton@gmail.com>
2022-06-15 21:42:56 +0600

    Code changes:  * Less nested `if` statements;  * Created `MOD_ID` variable to store mod identificator;  * Created a new function for searching the elevator block;  * Elevator now teleports exactly on the center of the elevator, preventing suffocation;  * Documentation;  * Sorted all imports.

--- src/main/java/net/imfalling/obelevator/Elevator.java
+++ src/main/java/net/imfalling/obelevator/Elevator.java
@@ -2,91 +2,114 @@ package net.imfalling.obelevator;
 
 import net.imfalling.obelevator.mixin.CooldownMixin;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Position;
 import net.minecraft.util.math.Vec3d;
 
 public class Elevator extends Block {
-    
+    static int MAXIMUM_TELEPORT_HEIGHT = 32;
+    int COOLDOWN = 0;
+
     public Elevator(Settings settings){
         super(settings);
     }
 
-    static int MAXIMUM_TELEPORT_HEIGHT = 32;
-    int COOLDOWN = 0;
+	/**
+	 * Attempts to find a suitable elevator to teleport the player to.
+	 * @param player A player, from which position to find the elevator block.
+	 * @param searchOnTop If true, will search for the elevator on top of the player, otherwise will search for the elevator below.
+	 * @return A block position of the elevator, or null if no elevator was found.
+	 */
+	public static BlockPos findElevatorBlock(PlayerEntity player, Boolean searchOnTop) {
+		// Go through blocks in range and check if they are the same type as the block over or below the player.
+		for (int i = (searchOnTop ? 2 : 4); i <= MAXIMUM_TELEPORT_HEIGHT; i++) {
+			int height = (searchOnTop ? i : -i);
 
-    static void playWoosh(PlayerEntity player){
-        if (!player.world.isClient) {
-            player.world.playSound(
-                    null, // Player - if non-null, will play sound for every nearby player *except* the specified player
-                    new BlockPos(player.getPos()), // The position of where the sound will come from
-                    ExampleMod.WOOSH_EVENT, // The sound that will play
-                    SoundCategory.BLOCKS, // This determines which of the volume sliders affect this sound
-                    1f, //Volume multiplier, 1 is normal, 0.5 is half volume, etc
-                    1f // Pitch multiplier, 1 is normal, 0.5 is half pitch, etc
-            );
-        }
+			// if searchOnTop is false, then we search for the block *below* the player.
+			// if searchOnTop is true, then we search for the block *above* the player.
+			BlockState blockUnderFeet = player.world.getBlockState(new BlockPos(player.getX(), player.getY() + height, player.getZ()));
+
+			// Check if we have got an Elevator block.
+			if (blockUnderFeet.getBlock() != ExampleMod.ELEVATOR_BLOCK) { continue; }
+
+			// ExampleMod.LOGGER.debug("Found!");
+			return new BlockPos(player.getX(), player.getY() + height, player.getZ());
+		}
+
+		// Return null if no elevator was found:
+		return null;
+	}
+
+	/**
+	 * Plays a "woosh!" sound.
+	 * @param player Player to play sound for.
+	 */
+    static void playWoosh(PlayerEntity player) {
+		// We should play the sound on server-side.
+		if (player.world.isClient()) { return; }
+
+		// Play the sound.
+        player.world.playSound(
+            null, // Player - if non-null, will play sound for every nearby player *except* the specified player
+            new BlockPos(player.getPos()), // The position of where the sound will come from
+            ExampleMod.WOOSH_EVENT, // The sound that will play
+            SoundCategory.BLOCKS, // This determines which of the volume sliders affect this sound
+            1f, //Volume multiplier, 1 is normal, 0.5 is half volume, etc
+            1f // Pitch multiplier, 1 is normal, 0.5 is half pitch, etc
+        );
     }
 
-    public static void onPlayerJump(LivingEntity living){
-        if(living instanceof PlayerEntity player){
-            //Find block under player
-            Position playerPos = new Vec3d(living.getX(), living.getY() -1, living.getZ());
-            Block underFeet = player.world.getBlockState(new BlockPos(playerPos)).getBlock();
-            int jumpingCooldown = ((CooldownMixin) player).getJumpingCooldown();
-            if(player.isOnGround() && jumpingCooldown == 0){
-                //If block is an elevator
-                if(underFeet instanceof Elevator elevator){
-                    //player.sendMessage(new LiteralText("Elevator!"), false);
-                    Block comparison;
-                    Position compcoord;
-                    //Find other elevator in range
-                    for(int i = 2; i <= MAXIMUM_TELEPORT_HEIGHT; i++){
-                        compcoord = new Vec3d(living.getX(), living.getY() + i, living.getZ());
-                        comparison = player.world.getBlockState(new BlockPos(compcoord)).getBlock();
-                        if(comparison.getClass() == underFeet.getClass()){
-                            //player.sendMessage(new LiteralText("Teleport!"), false);
-                            //Teleport!
-                            player.requestTeleport(compcoord.getX(), compcoord.getY()+1, compcoord.getZ());
-                            playWoosh(player);
-                            ((CooldownMixin)player).setJumpingCooldown(10);
+    public static void onPlayerJump(LivingEntity living) {
+		// Multiple checks:
+		//  - We should not use this function on client-side,
+		//  - only players can use the Elevator.
+		if (living.world.isClient() || living instanceof PlayerEntity player == false) { return; }
 
-                            break;
-                        }
-                    }
-                }
-            }
-        }  
+		// Set up variables:
+		PlayerEntity player = (PlayerEntity) living;
+		BlockState blockUnderFeet = player.world.getBlockState(new BlockPos(new Vec3d(living.getX(), living.getY() - 1, living.getZ())));
+		int jumpingCooldown = ((CooldownMixin) player).getJumpingCooldown();
+
+		// Only standing on elevator players with 0 cooldown can use the elevator:
+		if ((player.isOnGround() && jumpingCooldown == 0 && blockUnderFeet.getBlock() instanceof Elevator elevator) == false) { return; }
+
+		// Find the elevator on top of a player:
+		BlockPos elevatorPos = findElevatorBlock(player, true);
+
+		// If elevator was found, teleport the player:
+		if (elevatorPos == null) { return; }
+
+		// Teleport the player:
+		// ((CooldownMixin) player).setJumpingCooldown(1);
+		player.requestTeleportAndDismount(elevatorPos.getX() + 0.5, elevatorPos.getY() + 1, elevatorPos.getZ() + 0.5);
+		playWoosh(player);
     }
-    public static void onPlayerSneak(LivingEntity living){
-        if(living instanceof PlayerEntity player){
-            if(player.isSneaking()){
-                //Find block under player
-                Position playerPos = new Vec3d(living.getX(), living.getY() -1, living.getZ());
-                Block underFeet = player.world.getBlockState(new BlockPos(playerPos)).getBlock();
-                //If block is an elevator
-                if(underFeet instanceof Elevator elevator){
-                    //player.sendMessage(new LiteralText("Elevator!"), false);
-                    Block comparison;
-                    Position compcoord;
-                    //Find other elevator
-                    for(int i = 4; i <= MAXIMUM_TELEPORT_HEIGHT; i++){
-                        compcoord = new Vec3d(living.getX(), living.getY() - i, living.getZ());
-                        comparison = player.world.getBlockState(new BlockPos(compcoord)).getBlock();
-                        if(comparison.getClass() == underFeet.getClass()){
-                            //player.sendMessage(new LiteralText("Teleport!"), false);
-                            //Teleport
-                            player.requestTeleport(compcoord.getX(), compcoord.getY() + 1, compcoord.getZ());
-                            playWoosh(player);
-                            break;
-    
-                        }
-                    }
-                }
-            }
-        }
-    }
+
+    public static void onPlayerSneak(LivingEntity living) {
+		// Multiple checks:
+		//  - We should not use this function on client-side,
+		//  - only players can use the Elevator.
+		if (living.world.isClient() || living instanceof PlayerEntity player == false) { return; }
+
+		PlayerEntity player = (PlayerEntity) living;
+		BlockState blockUnderFeet = player.world.getBlockState(new BlockPos(new Vec3d(living.getX(), living.getY() - 1, living.getZ())));
+		int jumpingCooldown = ((CooldownMixin) player).getJumpingCooldown();
+
+		// Only standing on elevator players with 0 cooldown can use the elevator:
+		if ((player.isOnGround() && jumpingCooldown == 0 && blockUnderFeet.getBlock() instanceof Elevator elevator) == false) { return; }
+
+		// Find the elevator on top of a player:
+		BlockPos elevatorPos = findElevatorBlock(player, false);
+
+		// If elevator was found, teleport the player:
+		if (elevatorPos == null) { return; }
+
+		// Teleport the player:
+		// ((CooldownMixin) player).setJumpingCooldown(1);
+		player.requestTeleportAndDismount(elevatorPos.getX() + 0.5, elevatorPos.getY() + 1, elevatorPos.getZ() + 0.5);
+		playWoosh(player);
+	}
 }
